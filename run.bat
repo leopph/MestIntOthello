97538ca2d153cf087cf21f8aa3339b60fcb1341e
leopph <levente.loffler99@gmail.com>
2021-11-10 14:21:39 +0100

    First commit

---
+++ run.bat
@@ -0,0 +1,10 @@
+@echo off
+xcopy /q /y out\production\MestIntOthello\Agent.class .
+xcopy /q /y lib\game_engine.jar .
+if exist Agent.class (
+    if exist game_engine.jar (
+        java -jar game_engine.jar 10 game.oth.OthelloGame 1234567890 10 3 2000 game.oth.players.GreedyPlayer game.oth.players.GreedyPlayer
+        del game_engine.jar
+    )
+    del Agent.class
+)
---
+++ src/Agent.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+import game.oth.OthelloAction;
+import game.oth.OthelloPlayer;
+
+
+public class Agent extends OthelloPlayer
+{
+    public Agent(int color, int[][] board, Random random)
+    {
+        super(color, board, random);
+    }
+
+    @Override
+    public OthelloAction getAction(OthelloAction prevAction, long[] remainingTimes) {
+        int i = random.nextInt(board.length);
+        int j = random.nextInt(board[i].length);
+        return new OthelloAction(i, j);
+    }
+}
